7f4764a37325d8a10c4f5744fde5e6a4ca8a1169
eliezerantonio.com <eliezerfernandoantonio@gmail.com>
2021-03-27 02:45:34 +0100

    desafios2

---
+++ src/main/java/com/stackcode/gestao/controller/ClientesController.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.stackcode.gestao.controller;
+
+import com.stackcode.gestao.model.Produto;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author eliezer
+ */
+public class ClientesController {
+    
+    /*@RequestMapping("/clientes/novo")
+    public String novo(Produto produto){
+        return "cliente/CadastroCliente";
+    }
+    @RequestMapping(value="/produtos/novo", method=RequestMethod.POST)
+    public String cadastrar(@Valid Produto produto,BindResult result, MOdel model, RedirectAttributes attributes){
+        if(result.hasErrors()){
+            retunr novo(produto)
+        }
+    }
+    
+    */
+}
